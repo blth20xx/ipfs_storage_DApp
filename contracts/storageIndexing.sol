fb3104a3b057e69311084af92fbcab3b20d80c42
Javier <blth20xx@gmail.com>
2023-01-31 18:10:04 +0100

    supuesto penultimo commit

--- contracts/storageIndexing.sol
+++ contracts/storageIndexing.sol
@@ -8,34 +8,13 @@ contract storageIndexing {
         string name;
     }
 
-    mapping(address => fileMetadata[]) public files;
-
-    /* modifier onlyOwner (string calldata _filename) {
-        require(msg.sender == files[_filename].owner, "you are not the file owner");
-        _;
-    } */
-
+    mapping(address => fileMetadata[]) private files;
+    
+    
     function store(string calldata _newname, string calldata _newcid) public {
         files[msg.sender].push(fileMetadata(_newcid,_newname));
     }
 
-    function retrieve(string calldata _filename) public view returns (string memory cid){
-
-        for(uint8 i=0; i < files[msg.sender].length; i++){
-            if(keccak256(abi.encodePacked(files[msg.sender][i].name)) == keccak256(abi.encodePacked(_filename))){
-                return files[msg.sender][i].cid;
-            }
-        }
-        return "";
-    }
-
-    function clear(string calldata _filename) public {
-        for(uint8 i=0; i < files[msg.sender].length; i++){
-            if(keccak256(abi.encodePacked(files[msg.sender][i].name)) == keccak256(abi.encodePacked(_filename))){
-                files[msg.sender][i].cid = "";
-            }
-        }
-    }
 
     function retrieveAll() public view returns (fileMetadata[] memory){
         return files[msg.sender];
